82b6edf663cffe91538568d92af94a9f1fb79e73
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2016-12-10 10:19:29 -0500

    Comment mocks, added resolvers

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ import express from 'express';
 import { apolloServer } from 'graphql-tools';
 import Schema from './data/schema';
 import Mocks from './data/mocks';
+import Resolvers from './data/resolvers';
 
 const GRAPHQL_PORT = 8080;
 
 const graphQLServer = express();
-graphQLServer.use('/graphql', apolloServer({
+graphQLServer.use('/', apolloServer({
   graphiql: true,
   pretty: true,
   schema: Schema,
-  mocks: Mocks,
+  resolvers: Resolvers,
+  //mocks: Mocks,
 }));
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`
